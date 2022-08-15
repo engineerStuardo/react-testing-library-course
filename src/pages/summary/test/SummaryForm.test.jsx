95b56b6639cd0a0640147d62749d99a72b6ef885
engineerStuardo <ingeniero.stuardo@gmail.com>
2022-08-15 04:57:35 -0600

    adding test to popover

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import SummaryForm from '../SummaryForm';
 
@@ -23,13 +28,38 @@ test('Testing checkbox that enables button when checked and disables it when unc
   expect(button).toBeDisabled();
   expect(checkBox).not.toBeChecked();
 
-  fireEvent.click(checkBox);
+  userEvent.click(checkBox);
 
   expect(checkBox).toBeChecked();
   expect(button).toBeEnabled();
 
-  fireEvent.click(checkBox);
+  userEvent.click(checkBox);
 
   expect(button).toBeDisabled();
   expect(checkBox).not.toBeChecked();
+});
+
+test('popover responds to hover', async () => {
+  render(<SummaryForm />);
+
+  // popover starts out hidden
+  const popover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(popover).not.toBeInTheDocument();
+
+  // popover appears upon mouseover of checkbox label
+  const terms = screen.getByText(/terms and condition/i);
+  userEvent.hover(terms);
+
+  const popOverShow = screen.getByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(popOverShow).toBeInTheDocument();
+
+  //popover disappears when we mouse out
+  userEvent.unhover(terms);
+  await waitForElementToBeRemoved(
+    screen.queryByText(/no ice cream will actually be delivered/i)
+  );
 });
